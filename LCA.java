defda71032335f78701a6d8364e1feea63361d3f
Michael McKay <michaelmckay@Michaels-MacBook-Pro.local>
2018-10-25 17:10:23 +0100

    added node class to my LCA java program

---
+++ LCA.java
@@ -0,0 +1,18 @@
+//This is my Java Code for the Lowest Common Ancestor Problem
+//My graphh is structured as a binary tree
+
+import java.util.List;
+import java.util.ArrayList;
+
+//add a node
+// v is equal to a value
+
+class node{
+	int data, v;
+	Node left, right;
+
+	Node(x){
+		data = x;
+		left = right = null;
+	}	
+} 
